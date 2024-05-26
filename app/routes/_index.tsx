ed111e479a45f8558ba2d8d1495a6836f2e9a26d
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-26 17:33:37 -0300

    add ui components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,7 @@ import { Button } from "~/components/ui/button";
 export default function Index() {
   return (
     <div>
+      <title>Admin Panel RR</title>
       <div>
         <Button>Click me</Button>
       </div>
