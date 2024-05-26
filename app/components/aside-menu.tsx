9cda6873a13beed65d437abb39a44256439719fc
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-26 18:37:09 -0300

    feat: Update dashboard routes and page titles
    This commit updates the dashboard routes to include the respective page titles for "Customers" and "Inventory". This improves the user experience by providing clear and descriptive titles for each dashboard page.

--- app/components/aside-menu.tsx
+++ app/components/aside-menu.tsx
@@ -8,7 +8,7 @@ import {
   ShoppingCart,
   Users,
 } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { Button } from "./ui/button";
 import {
   Card,
@@ -17,15 +17,42 @@ import {
   CardDescription,
   CardContent,
 } from "./ui/card";
+import { firstLetterUpperCase } from "utils/strings-utils";
+import clsx from "clsx";
 
 export default function AsideMenu() {
+  const routesLinks = [
+    {
+      route: "home",
+      icon: <Home className="h-4 w-4" />,
+    },
+    {
+      route: "products",
+      icon: <Package className="h-4 w-4" />,
+    },
+    {
+      route: "customers",
+      icon: <Users className="h-4 w-4" />,
+    },
+    {
+      route: "inventory",
+      icon: <LineChart className="h-4 w-4" />,
+    },
+  ];
+
+  const location = useLocation();
+
+  const isRouteActive = (route: string) => {
+    return location.pathname.includes(route);
+  };
+
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link to="/" className="flex items-center gap-2 font-semibold">
             <Package2 className="h-6 w-6" />
-            <span className="">Acme Inc</span>
+            <span className="">Admin Panel</span>
           </Link>
           <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
             <Bell className="h-4 w-4" />
@@ -34,34 +61,18 @@ export default function AsideMenu() {
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <Link
-              to="home"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Home className="h-4 w-4" />
-              Home
-            </Link>
-            <Link
-              to="products"
-              className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-            >
-              <Package className="h-4 w-4" />
-              Products{" "}
-            </Link>
-            <Link
-              to="customers"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Users className="h-4 w-4" />
-              Customers
-            </Link>
-            <Link
-              to="inventory"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <LineChart className="h-4 w-4" />
-              Inventory
-            </Link>
+            {routesLinks.map((route) => (
+              <Link
+                to={route.route}
+                className={clsx(
+                  "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+                  { "bg-muted text-primary": isRouteActive(route.route) }
+                )}
+              >
+                {route.icon}
+                {firstLetterUpperCase(route.route)}
+              </Link>
+            ))}
           </nav>
         </div>
         <div className="mt-auto p-4">
